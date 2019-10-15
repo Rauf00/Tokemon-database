427d7f45c1670d54d12fb4e82f8cce17fd87b40e
Rauf Shimarov <shimarov00@gmail.com>
2019-10-15 14:43:00 -0700

    Assignment2 v4

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 5000
 var app = express();
 
 const { Pool } = require('pg');
-var pool;
-pool = new Pool({
-  connectionString: "postgres://postgres:shimarov6929@localhost/assignment2"
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,6 +15,18 @@ app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {res.render('pages/index')});
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 app.get('/hello', (req,res) => { res.render('pages/hello')});
 app.get('/assignment2', (req,res) => { res.render('pages/tokemon')});
 
